2a9e8ecabacac5b3fed34dfcb88e815f92118e91
Lars Nysom <LANY@ucn.dk>
2022-09-05 13:24:28 +0200

    Removed Main method to simplify code

--- AbstractVirtual/Program.cs
+++ AbstractVirtual/Program.cs
@@ -2,23 +2,14 @@
 using System;
 using System.Collections.Generic;
 
-namespace AbstractVirtual
+// TODO: Instantiate the animal objects and insert them into the list
+
+List<Animal> animals = new() { /* replace this with the animal objects */ };
+
+foreach (Animal ani in animals)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // TODO: Instantiate the animal objects and insert them into the list
-
-            List<Animal> animals = new List<Animal>() { /* replace this with the animal objects */ };
-
-            foreach (Animal ani in animals)
-            {
-                Console.WriteLine(ani);
-                ani.PerformTrick();
-                ani.FeedAnimal();
-            }
-            Console.ReadLine();
-        }
-    }
+    Console.WriteLine(ani);
+    ani.PerformTrick();
+    ani.FeedAnimal();
 }
+Console.ReadLine();
